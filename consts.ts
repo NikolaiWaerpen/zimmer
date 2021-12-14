6098c6d780b8b385c84a7cde336df870be0c2a4c
Nikolai Waerpen <nikolai.waerpen@intility.no>
2021-12-14 14:59:45 +0100

    fix: add prod url

--- consts.ts
+++ consts.ts
@@ -1,6 +1,6 @@
 export const URL =
   process.env.NODE_ENV === "production"
-    ? "INPUT CORRECT URL WHEN PRODUCTION BACKEND IS UP" // TODO: this
+    ? process.env.BACKEND_URL
     : "http://localhost:4000";
 
 export const MAX_GREETING_TITLE_LENGTH = 25,
