27ba70e5c68c77d19768fef3c6be994ae1b8c0ae
Nikolai Waerpen <nikolai.waerpen@intility.no>
2021-12-19 01:20:08 +0100

    fix: remove unused <p />

--- pages/nft/[assets]/index.tsx
+++ pages/nft/[assets]/index.tsx
@@ -72,7 +72,7 @@ export default function Assets() {
                     <div>
                       <h3 className="mt-4 text-sm text-gray-700">{name}</h3>
                       {/* <p className="mt-1 text-lg font-medium text-gray-900">
-                        {/* {product.price} */}test
+                        test
                       </p> */}
                     </div>
                   </div>
