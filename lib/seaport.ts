6976ec82c8e0a1b8d64810f9376b2eb01268ba80
Nikolai Waerpen <nikolai.waerpen@intility.no>
2021-12-21 17:47:07 +0100

    fix: remove seaport client

--- lib/seaport.ts
+++
@@ -1,9 +0,0 @@
-import { Network, OpenSeaPort } from "opensea-js";
-import Web3 from "web3";
-
-const provider = new Web3.providers.HttpProvider("https://mainnet.infura.io");
-
-export const seaport = new OpenSeaPort(provider, {
-  networkName: Network.Main,
-  apiKey: "",
-});
