2cf1896b102fbaf57f6719f1892efd985e2b5d13
NikolaiWaerpen <nikolaiwaerpen@gmail.com>
2022-01-17 18:16:54 +0100

    style: make current nav fixed

--- components/Layout/Authenticated.tsx
+++ components/Layout/Authenticated.tsx
@@ -45,97 +45,99 @@ export default function Authenticated({ children }: AuthenticatedProps) {
   return (
     <div>
       {/* Top navigation */}
-      <Popover className="relative bg-white">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6">
-          <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
-            <div className="flex justify-start lg:w-0 lg:flex-1">
-              <a href="/">
-                <span className="sr-only">Workflow</span>
-                <img
-                  className="h-8 w-auto sm:h-10"
-                  src="images/logo.png"
-                  alt="logo"
-                />
-              </a>
-            </div>
-            <div className="-mr-2 -my-2 md:hidden">
-              <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-themecolor3">
-                <span className="sr-only">Open menu</span>
-                <MenuIcon className="h-6 w-6" aria-hidden="true" />
-              </Popover.Button>
-            </div>
-            <Popover.Group as="nav" className="hidden md:flex space-x-10">
-              {NAVIGATION.main.map(({ name, href }) => (
-                <a
-                  key={name}
-                  href={href}
-                  className="text-base font-medium text-gray-500 hover:text-gray-900"
-                >
-                  {name}
+      <div className="top-0 left-0 fixed w-full z-[420]">
+        <Popover className="relative bg-white">
+          <div className="max-w-7xl mx-auto px-4 sm:px-6">
+            <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
+              <div className="flex justify-start lg:w-0 lg:flex-1">
+                <a href="/">
+                  <span className="sr-only">Workflow</span>
+                  <img
+                    className="h-8 w-auto sm:h-10"
+                    src="images/logo.png"
+                    alt="logo"
+                  />
                 </a>
-              ))}
-            </Popover.Group>
-            <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-              <Profile />
+              </div>
+              <div className="-mr-2 -my-2 md:hidden">
+                <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-themecolor3">
+                  <span className="sr-only">Open menu</span>
+                  <MenuIcon className="h-6 w-6" aria-hidden="true" />
+                </Popover.Button>
+              </div>
+              <Popover.Group as="nav" className="hidden md:flex space-x-10">
+                {NAVIGATION.main.map(({ name, href }) => (
+                  <a
+                    key={name}
+                    href={href}
+                    className="text-base font-medium text-gray-500 hover:text-gray-900"
+                  >
+                    {name}
+                  </a>
+                ))}
+              </Popover.Group>
+              <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
+                <Profile />
+              </div>
             </div>
           </div>
-        </div>
 
-        <Transition
-          as={Fragment}
-          enter="duration-200 ease-out"
-          enterFrom="opacity-0 scale-95"
-          enterTo="opacity-100 scale-100"
-          leave="duration-100 ease-in"
-          leaveFrom="opacity-100 scale-100"
-          leaveTo="opacity-0 scale-95"
-        >
-          <Popover.Panel
-            focus
-            className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
+          <Transition
+            as={Fragment}
+            enter="duration-200 ease-out"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="duration-100 ease-in"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
           >
-            <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
-              <div className="pt-5 pb-6 px-5">
-                <div className="flex items-center justify-between">
+            <Popover.Panel
+              focus
+              className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
+            >
+              <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
+                <div className="pt-5 pb-6 px-5">
+                  <div className="flex items-center justify-between">
+                    <div>
+                      <img
+                        className="h-8 w-auto"
+                        src="images/logo.png"
+                        alt="logo"
+                      />
+                    </div>
+                    <div className="-mr-2">
+                      <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-themecolor3">
+                        <span className="sr-only">Close menu</span>
+                        <XIcon className="h-6 w-6" aria-hidden="true" />
+                      </Popover.Button>
+                    </div>
+                  </div>
+                </div>
+                <div className="py-6 px-5 space-y-6">
+                  <div className="grid grid-cols-2 gap-y-4 gap-x-8">
+                    {NAVIGATION.main.map(({ name, href }) => (
+                      <a
+                        key={name}
+                        href={href}
+                        className="text-base font-medium text-gray-900 hover:text-gray-700"
+                      >
+                        {name}
+                      </a>
+                    ))}
+                  </div>
                   <div>
-                    <img
-                      className="h-8 w-auto"
-                      src="images/logo.png"
-                      alt="logo"
-                    />
-                  </div>
-                  <div className="-mr-2">
-                    <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-themecolor3">
-                      <span className="sr-only">Close menu</span>
-                      <XIcon className="h-6 w-6" aria-hidden="true" />
-                    </Popover.Button>
+                    <Profile />
                   </div>
                 </div>
               </div>
-              <div className="py-6 px-5 space-y-6">
-                <div className="grid grid-cols-2 gap-y-4 gap-x-8">
-                  {NAVIGATION.main.map(({ name, href }) => (
-                    <a
-                      key={name}
-                      href={href}
-                      className="text-base font-medium text-gray-900 hover:text-gray-700"
-                    >
-                      {name}
-                    </a>
-                  ))}
-                </div>
-                <div>
-                  <Profile />
-                </div>
-              </div>
-            </div>
-          </Popover.Panel>
-        </Transition>
-      </Popover>
+            </Popover.Panel>
+          </Transition>
+        </Popover>
+      </div>
 
       {/* Container */}
       <div className="container max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="mt-8 h-screen">{children}</div>
+        <div className="mt-24 h-screen">{children}</div>
       </div>
 
       {/* Footer */}
