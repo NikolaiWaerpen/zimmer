476a859767a9237a4a8bdf3cd26a7c7b6befd90e
Nikolai Waerpen <nikolaiwaerpen@gmail.com>
2022-01-23 21:04:37 +0100

    feat: hero section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,39 @@
+import heroAnimation from "public/lottie/hero.json";
+import Lottie from "lottie-react";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div className="bg-white">
-      <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
-        <div className="text-center">
-          <p className="mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">
-            Die Residenz
-          </p>
-          <p className="max-w-xl mt-5 mx-auto text-xl text-gray-500">
-            Willkommen in meiner Residenz, bestehend aus Zimmer und Keller
-          </p>
+    <main className="lg:mt-36 lg:relative">
+      <div className="mx-auto max-w-7xl w-full pt-16 pb-20 text-center lg:py-48 lg:text-left">
+        <div className="px-4 lg:w-1/2 sm:px-8 xl:pr-16">
+          <div className="flex flex-col gap-4">
+            <span className="text-lg text-theme-5 sm:text-xl md:mt-5 md:max-w-3xl">
+              Hi, my name is
+            </span>
+            <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
+              Nikolai Wærpen.
+            </h1>
+            <h1 className="text-4xl tracking-tight font-bold sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl text-gray-500">
+              I build things.
+            </h1>
+          </div>
+          {/* <div className="mt-10 sm:flex sm:justify-center lg:justify-start">
+            <div className="rounded-md shadow-lg">
+              <Link href="/explore-collections">
+                <a className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
+                  Explore
+                </a>
+              </Link>
+            </div>
+          </div>*/}
         </div>
       </div>
-    </div>
+      <div className="relative w-full h-64 sm:h-72 md:h-96 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 lg:h-full">
+        <div className="absolute inset-0 w-full h-full">
+          <Lottie animationData={heroAnimation} loop className="sm:mt-28" />
+        </div>
+      </div>
+    </main>
   );
 }
