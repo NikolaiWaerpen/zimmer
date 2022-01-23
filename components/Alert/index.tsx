4bfc7d3568115b65db5c3fa79846b7f09edb8b95
Nikolai Waerpen <nikolaiwaerpen@gmail.com>
2022-01-23 16:40:25 +0100

    fix: state rework

--- components/Alert/index.tsx
+++ components/Alert/index.tsx
@@ -1,8 +1,13 @@
-import { ExclamationIcon } from "@heroicons/react/solid";
+import {
+  CheckCircleIcon,
+  ExclamationIcon,
+  InformationCircleIcon,
+  XCircleIcon,
+} from "@heroicons/react/solid";
 import classNames from "classnames";
 
 type AlertProps = {
-  state?: "default" | "warning" | "error";
+  state?: "default" | "success" | "warning" | "error";
   title: string;
   description?: string;
 };
@@ -12,32 +17,50 @@ export default function Alert({
   title,
   description,
 }: AlertProps) {
+  const isDefault = state == "default";
+  const isSuccess = state == "success";
+  const isWarning = state == "warning";
+  const isError = state == "error";
+
   return (
     // TODO: FIX coloring on success state
     <div
       className={classNames("rounded-md p-4", {
-        "bg-theme-4": state === "default",
-        "bg-yellow-50": state === "warning",
-        "bg-red-50": state === "error",
+        "bg-theme-4": isDefault,
+        "bg-yellow-50": isWarning,
+        "bg-red-50": isError,
       })}
     >
       <div className="flex">
         <div className="flex-shrink-0">
-          <ExclamationIcon
-            className={classNames("h-5 w-5", {
-              "text-theme-6": state === "default",
-              "text-yellow-400": state === "warning",
-              "text-red-400": state === "error",
-            })}
-            aria-hidden="true"
-          />
+          {isDefault && (
+            <InformationCircleIcon
+              className="h-5 w-5 text-theme-6"
+              aria-hidden="true"
+            />
+          )}
+          {isSuccess && (
+            <CheckCircleIcon
+              className="h-5 w-5 text-green-400"
+              aria-hidden="true"
+            />
+          )}
+          {isWarning && (
+            <ExclamationIcon
+              className="h-5 w-5 text-yellow-400"
+              aria-hidden="true"
+            />
+          )}
+          {isError && (
+            <XCircleIcon className="h-5 w-5 text-red-400" aria-hidden="true" />
+          )}
         </div>
         <div className="ml-3">
           <h3
             className={classNames("text-sm font-medium", {
-              "text-theme-7": state === "default",
-              "text-yellow-800": state === "warning",
-              "text-red-800": state === "error",
+              "text-theme-7": isDefault,
+              "text-yellow-800": isWarning,
+              "text-red-800": isError,
             })}
           >
             {title}
@@ -45,9 +68,9 @@ export default function Alert({
           {description && (
             <div
               className={classNames("mt-2 text-sm", {
-                "text-theme-6": state === "default",
-                "text-yellow-700": state === "warning",
-                "text-red-700": state === "error",
+                "text-theme-6": isDefault,
+                "text-yellow-700": isWarning,
+                "text-red-700": isError,
               })}
             >
               <p>{description}</p>
