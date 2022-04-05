7f738805a92ed22e0a05b87b9c1205118aa1e3d4
NikolaiWaerpen <nikolaiwaerpen@gmail.com>
2022-04-05 20:18:12 +0200

    fix: transparent bug on mobile

--- components/Birds/index.tsx
+++ components/Birds/index.tsx
@@ -7,19 +7,19 @@ export default function Birds() {
       <div className="w-full h-full relative">
         {/* LEFT BORDER */}
         <div className="absolute left-0 right-[95%] bottom-0 top-0">
-          <div className="w-full h-full bg-gradient-to-l from-real-transparent to-real-white absolute"></div>
+          <div className="w-full h-full bg-gradient-to-r from-real-white absolute"></div>
         </div>
         {/* RIGHT BORDER */}
         <div className="absolute right-0 left-[95%] bottom-0 top-0">
-          <div className="w-full h-full bg-gradient-to-r from-real-transparent to-real-white absolute"></div>
+          <div className="w-full h-full bg-gradient-to-l from-real-white absolute"></div>
         </div>
         {/* TOP BORDER */}
         <div className="absolute right-0 left-0 bottom-[90%] top-0">
-          <div className="mt-16 w-full h-full bg-gradient-to-t from-real-transparent to-real-white absolute"></div>
+          <div className="mt-16 w-full h-full bg-gradient-to-b from-real-white absolute"></div>
         </div>
         {/* BOTTOM BORDER */}
         <div className="absolute right-0 left-0 bottom-0 top-[70%]">
-          <div className="w-full h-full bg-gradient-to-b from-real-transparent to-real-white absolute"></div>
+          <div className="w-full h-full bg-gradient-to-t from-real-white absolute"></div>
         </div>
         {/* DIV MAKING BIRDS LESS OPAQUE */}
         <div className="w-full h-full bg-white opacity-[0.2] absolute" />
