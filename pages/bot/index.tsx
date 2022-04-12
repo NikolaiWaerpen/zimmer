9b4b21b7385bd5a3817eda621784425b7e7abaa7
NikolaiWaerpen <nikolaiwaerpen@gmail.com>
2022-04-13 00:23:51 +0200

    feat: several wallets in overview

--- pages/bot/index.tsx
+++ pages/bot/index.tsx
@@ -1,6 +1,6 @@
 import { gql, useQuery } from "@apollo/client";
-import NetProfitTotal from "components/bot/NetProfitTotal";
-import StatisticsBox from "components/bot/StatisticsBox";
+import NetProfitTotal from "components/trade/NetProfitTotal";
+import StatisticsBox from "components/trade/StatisticsBox";
 import CustomError from "components/CustomError";
 import Loader from "components/Loader";
 import formatDate from "utils/format-date";
@@ -17,7 +17,6 @@ const GET_TRADES = gql`
       sell
       sellDate
       profit
-      nokProfit
     }
   }
 `;
@@ -29,10 +28,9 @@ type BotTradesType = {
   fees: string;
   buy: string;
   buyDate: string;
-  sell: string;
-  sellDate: string;
-  profit: string;
-  nokProfit: string;
+  sell: string | null;
+  sellDate: string | null;
+  profit: string | null;
 };
 
 export default function Bot() {
@@ -41,8 +39,10 @@ export default function Bot() {
     {
       variables: {
         input: {
-          // address: "0x1022E8731677efC814b937FB0d1AFc83D9773b20",
-          address: "0x9570c76b22D0654F0c21903Be86a38e4eB5550B4",
+          addresses: [
+            "0x9570c76b22D0654F0c21903Be86a38e4eB5550B4",
+            "0x1022E8731677efC814b937FB0d1AFc83D9773b20",
+          ],
         },
       },
     }
@@ -53,24 +53,30 @@ export default function Bot() {
 
   const { botTrades } = data;
 
+  const netProfit = botTrades
+    .map(({ profit }) => profit)
+    .reduce((accumulator, current) => {
+      if (current) accumulator += +current;
+      return accumulator;
+    }, 0)
+    .toFixed(3);
+
   return (
-    <div>
-      <br />
+    <div className="pt-8">
+      <div className="sm:flex sm:items-center">
+        <div className="sm:flex-auto">
+          <h1 className="text-xl font-semibold text-gray-900">Bot trades</h1>
+          <p className="mt-2 text-sm text-gray-700">All trades by bots</p>
+        </div>
+      </div>
 
       <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
-        <NetProfitTotal />
-        <StatisticsBox name="Average profit margin" stat="11.3%" />
-        <StatisticsBox name="Wallet balance" stat="23012 kr" />
+        <StatisticsBox name="Net profit" stat={netProfit + " ETH"} />
+        <StatisticsBox name="Some cool stat" stat={"0"} />
+        <StatisticsBox name="Another cool stat" stat={"0"} />
       </dl>
-      <br />
-      <div className="px-4 sm:px-6 lg:px-8">
-        <div className="sm:flex sm:items-center">
-          <div className="sm:flex-auto">
-            <h1 className="text-xl font-semibold text-gray-900">Bot trades</h1>
-            <p className="mt-2 text-sm text-gray-700">All trades by bot</p>
-          </div>
-        </div>
 
+      <div>
         <div className="mt-8 flex flex-col">
           <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
@@ -129,7 +135,6 @@ export default function Bot() {
                           sell,
                           sellDate,
                           profit,
-                          nokProfit,
                         },
                         idx
                       ) => (
@@ -140,22 +145,28 @@ export default function Bot() {
                           <td className="whitespace-nowrap py-2 pl-4 pr-3 text-sm text-gray-500 sm:pl-6">
                             {collection}
                           </td>
-                          <td className="whitespace-nowrap px-2 py-2 text-sm font-medium text-gray-900">
+                          <td className="whitespace-nowrap px-2 py-2 text-sm font-medium text-gray-500">
                             <a href={link} target="_blank" rel="noreferrer">
                               {tokenId}
                             </a>
                           </td>
-                          <td className="whitespace-nowrap px-2 py-2 text-sm font-medium text-gray-900">
+                          <td className="whitespace-nowrap px-2 py-2 text-sm font-medium text-gray-500">
                             {fees}
                           </td>
                           <td className="whitespace-nowrap px-2 py-2 text-sm text-gray-500">
                             {buy} ({formatDate({ date: new Date(buyDate) })})
                           </td>
                           <td className="whitespace-nowrap px-2 py-2 text-sm text-gray-500">
-                            {sell} ({formatDate({ date: new Date(sellDate) })})
+                            {sell ? sell : "N/A"}
+                            {sellDate && (
+                              <span>
+                                {" "}
+                                ({formatDate({ date: new Date(sellDate) })})
+                              </span>
+                            )}
                           </td>
-                          <td className="whitespace-nowrap px-2 py-2 text-sm text-gray-500">
-                            {profit} ({nokProfit})
+                          <td className="whitespace-nowrap px-2 py-2 text-sm text-gray-900">
+                            {profit ? profit : "N/A"}
                           </td>
                         </tr>
                       )
