bf2136a13e3ffde30894506fe92acdc9492feee6
NikolaiWaerpen <nikolaiwaerpen@gmail.com>
2022-04-13 14:29:22 +0200

    style: size changes

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -26,22 +26,24 @@ export default function Button({
   className,
 }: ButtonProps) {
   return (
-    <button
-      onClick={onClick}
-      type={type}
-      className={`${className} inline-flex items-center px-3 py-3 border border-transparent text-sm leading-4 font-medium rounded-md shadow-sm  focus:outline-none focus:ring-2 focus:ring-offset-2 ${
-        disabled
-          ? "focus:ring-gray-500 text-white bg-gray-600 hover:bg-gray-700 hover:cursor-not-allowed"
-          : "focus:ring-theme-3 text-white bg-theme-4 hover:bg-theme-5"
-      } `}
-    >
-      <div className="flex justify-center items-center gap-2 w-full">
-        {/* Default icon */}
-        {icon && !rightIcon && <FontAwesomeIcon icon={icon} />}
-        {loading ? <ButtonSpinner /> : <>{children}</>}
-        {/* Right icon */}
-        {icon && rightIcon && <FontAwesomeIcon icon={icon} />}
-      </div>
-    </button>
+    <div>
+      <button
+        onClick={onClick}
+        type={type}
+        className={`${className} inline-flex items-center px-[0.920rem] py-[0.920rem] border border-transparent text-sm leading-4 font-medium rounded-md shadow-sm  focus:outline-none focus:ring-2 focus:ring-offset-2 ${
+          disabled
+            ? "focus:ring-gray-500 text-white bg-gray-600 hover:bg-gray-700 hover:cursor-not-allowed"
+            : "focus:ring-theme-3 text-white bg-theme-4 hover:bg-theme-5"
+        } `}
+      >
+        <div className="flex justify-center items-center gap-2 w-full">
+          {/* Default icon */}
+          {icon && !rightIcon && <FontAwesomeIcon icon={icon} />}
+          {loading ? <ButtonSpinner /> : <>{children}</>}
+          {/* Right icon */}
+          {icon && rightIcon && <FontAwesomeIcon icon={icon} />}
+        </div>
+      </button>
+    </div>
   );
 }
