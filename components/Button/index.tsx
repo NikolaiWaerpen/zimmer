8529825eb07f35e2144d40448efcf3647744734a
NikolaiWaerpen <nikolaiwaerpen@gmail.com>
2022-04-27 20:03:34 +0200

    style: tweak height

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -30,7 +30,7 @@ export default function Button({
       <button
         onClick={onClick}
         type={type}
-        className={`${className} inline-flex items-center px-[0.920rem] py-[0.920rem] border border-transparent text-sm leading-4 font-medium rounded-md shadow-sm  focus:outline-none focus:ring-2 focus:ring-offset-2 ${
+        className={`${className} inline-flex items-center px-[0.925rem] py-[0.87rem] border border-transparent text-sm leading-4 font-medium rounded-md shadow-sm  focus:outline-none focus:ring-2 focus:ring-offset-2 ${
           disabled
             ? "focus:ring-gray-500 text-white bg-gray-600 hover:bg-gray-700 hover:cursor-not-allowed"
             : "focus:ring-theme-3 text-white bg-theme-4 hover:bg-theme-5"
