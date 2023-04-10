6300debb5ca797f95f1fd0be953303a0158b4f26
NikolaiWaerpen <nikolaiwaerpen@gmail.com>
2023-04-10 02:03:42 +0200

    feat: webflow https proxy

---
+++ pages/api/webflow.ts
@@ -0,0 +1,13 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const response = await fetch("http://nikolaiwaerpen.ddns.net:8000/");
+  const rawJs = await response.text();
+
+  res.setHeader("Content-Type", "text/html; charset=utf-8");
+  res.write(rawJs);
+  res.end();
+}
